b4abfca831969b68cd9761958d7518a49a724143
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2023-04-25 00:58:27 -0400

    Setup omegaconf (#3)
    * etup OmegaConf for innit_transform
    
    * Setup omegaconf for datasets
    
    * Setup omegaconf for model

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from configs import load_config
 from datasets import init_dataset
 from models import init_model
 from transforms import init_transform
 
 
-def main():
-    model = init_model("ResNet20")
-    train_transform = init_transform()
-    train_dataset = init_dataset()
+def main(CONFIG):
+    model = init_model(CONFIG.model)
+    train_transform = init_transform(CONFIG.transforms.train)
+    valid_transform = init_transform(CONFIG.transforms.valid)
+    train_dataset = init_dataset(CONFIG.datasets.train, train_transform, train=True)
+    valid_dataset = init_dataset(CONFIG.datasets.valid, valid_transform, train=False)
 
     print(model)
+    print(train_transform)
+    print(valid_transform)
 
 
 if __name__ == "__main__":
-    main()
+    CONFIG = load_config()
+    main(CONFIG)
--- models/resnet.py
+++ models/resnet.py
@@ -1,56 +1,65 @@
+"""This file defines the ResNet architecture for CIFAR-10/100 dataset.
+
+Modified from akamaster/pytorch_resnet_cifar10
 """
-Properly implemented ResNet-s for CIFAR10 as described in paper [1].
-
-The implementation and structure of this file is hugely influenced by [2]
-which is implemented for ImageNet and doesn't have option A for identity.
-Moreover, most of the implementations on the web is copy-paste from
-torchvision's resnet and has wrong number of params.
-
-Proper ResNet-s for CIFAR10 (for fair comparision and etc.) has following
-number of layers and parameters:
-
-name      | layers | params
-ResNet20  |    20  | 0.27M
-ResNet32  |    32  | 0.46M
-ResNet44  |    44  | 0.66M
-ResNet56  |    56  | 0.85M
-ResNet110 |   110  |  1.7M
-ResNet1202|  1202  | 19.4m
-
-which this implementation indeed has.
-
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-[2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
-
-If you use this implementation in you work, please don't forget to mention the
-author, Yerlan Idelbayev.
-"""
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
 
-def _weights_init(m):
+def _weights_init(m: nn.Module):
+    """Initialize weights with Kaiming He initialization for ReLU networks.
+
+    Args:
+        m: Module to initialize weights.
+    """
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
 
 class LambdaLayer(nn.Module):
+    """Module encapsulating a Python method.
+
+    Attributes:
+        lambd: Method to be encapsulated.
+    """
+
     def __init__(self, lambd):
+        """Initialize module.
+
+        Args:
+            lambd: Method to be encapsulated.
+        """
         super(LambdaLayer, self).__init__()
         self.lambd = lambd
 
     def forward(self, x):
+        """Forward pass."""
         return self.lambd(x)
 
 
 class BasicBlock(nn.Module):
+    """Building block for ResNet.
+
+    Attributes:
+        conv1: First convolutional layer.
+        bn1: First batch normalization layer.
+        conv2: Second convolutional layer.
+        bn2: Second batch normalization layer.
+        shortcut: Residual layer(s).
+    """
+
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1, option="A"):
+        """Initialize ResNet building block.
+
+        Args:
+            in_planes: Number of input filters for first convolutional layer.
+            planes: Number of output filters for first convolutional layer.
+            stride: Stride for first convolutiona layer.
+            option: Shortcut option.
+        """
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
@@ -88,6 +97,7 @@ class BasicBlock(nn.Module):
                 )
 
     def forward(self, x):
+        """Forward pass."""
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
@@ -96,7 +106,26 @@ class BasicBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    """Residual Network for CIFAR10/100 datasets.
+
+    Attributes:
+        in_planes: Number of filters for first convolutional layer of ResNet blocks.
+        conv1: First convolutional layer.
+        bn1: First batch normalization layer.
+        layer1: First ResNet block group.
+        layer2: Second ResNet block group.
+        layer3: Third ResNet block group.
+        linear: Final linear layer.
+    """
+
+    def __init__(self, num_blocks, block=BasicBlock, num_classes=10):
+        """Initialize ResNet.
+
+        Args:
+            num_blocks: Number of blocks stacked for each ResNet block group.
+            block: Class for ResNet block.
+            num_classes: Number of classes to predict from.
+        """
         super(ResNet, self).__init__()
         self.in_planes = 16
 
@@ -110,6 +139,17 @@ class ResNet(nn.Module):
         self.apply(_weights_init)
 
     def _make_layer(self, block, planes, num_blocks, stride):
+        """Initialize ResNet building block.
+
+        Args:
+            block: Class for ResNet block.
+            planes: Number of output filters for first block.
+            num_blocks: Number of blocks to create for this ResNet block group.
+            stride: Stride to create each block.
+
+        Returns:
+            Group of ResNet blocks.
+        """
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
@@ -119,6 +159,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        """Forward pass."""
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
@@ -127,7 +168,3 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
-
-
-def init_ResNet20():
-    return ResNet(BasicBlock, [3, 3, 3])
