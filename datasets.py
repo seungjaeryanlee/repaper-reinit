024f49b0094903f04a635e1907f1b14375cc7abd
Ryan Lee <seungjaeryanlee@gmail.com>
2023-04-25 00:42:14 -0400

    etup OmegaConf for innit_transform

--- datasets.py
+++ datasets.py
@@ -1,9 +1,9 @@
 from torchvision.datasets import CIFAR10
 
 
-def init_dataset():
+def init_dataset(transform, train):
     dataset = CIFAR10(
-        root="./data/", train=True, transform=train_transform, download=True
+        root="./data/", train=train, transform=transform, download=True
     )
 
     return dataset
