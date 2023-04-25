3fcae4465a30d7390d498cffa166149834a1a73f
Ryan Lee <seungjaeryanlee@gmail.com>
2023-04-24 20:29:38 -0400

    Remove unused code

--- models/resnet.py
+++ models/resnet.py
@@ -32,22 +32,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
-from torch.autograd import Variable
-
-__all__ = [
-    "ResNet",
-    "resnet20",
-    "resnet32",
-    "resnet44",
-    "resnet56",
-    "resnet110",
-    "resnet1202",
-]
-
 
 def _weights_init(m):
-    classname = m.__class__.__name__
-    # print(classname)
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
@@ -165,32 +151,3 @@ def resnet110():
 
 def resnet1202():
     return ResNet(BasicBlock, [200, 200, 200])
-
-
-def test(net):
-    import numpy as np
-
-    total_params = 0
-
-    for x in filter(lambda p: p.requires_grad, net.parameters()):
-        total_params += np.prod(x.data.numpy().shape)
-    print("Total number of params", total_params)
-    print(
-        "Total layers",
-        len(
-            list(
-                filter(
-                    lambda p: p.requires_grad and len(p.data.size()) > 1,
-                    net.parameters(),
-                )
-            )
-        ),
-    )
-
-
-if __name__ == "__main__":
-    for net_name in __all__:
-        if net_name.startswith("resnet"):
-            print(net_name)
-            test(globals()[net_name]())
-            print()
