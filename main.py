e60576e6bcc1f4d8cde4530f4709ac30b3a79be6
Ryan Lee <seungjaeryanlee@gmail.com>
2023-04-24 23:32:55 -0400

    Add CIFAR-100 dataset

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from models import init_model
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
 
 
 def main():
     model = init_model("ResNet20")
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),
+        ]
+    )
+    train_dataset = CIFAR10(
+        root="./data/", train=True, transform=train_transform, download=True
+    )
+
     print(model)
 
 
